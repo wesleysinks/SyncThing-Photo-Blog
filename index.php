080c3f0c9e29d5b9e1244d9235706d746b38dd3d
Wesley Sinks <wesleysinks@gmail.com>
2017-11-10 20:19:53 -0500

    Added gitignore for posts and README for posts dir

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$title="Syncthing Photo Blog";
+$description="Proof of concept for a simple Syncthing powered photo blog";
+$author="Wesley Sinks"
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+
+    <title><?php echo $title ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?php echo $description ?>">
+    <meta name="author" content="<?php echo $author ?>">
+  </head>
+  <body>
+    <header id="mainHeader">
+      <h1 id="siteTitle"><?php echo $title ?></h1>
+      <p id="siteDesc"><?php echo $description ?></p>
+    </header>
+
+  </body>
+</html>
